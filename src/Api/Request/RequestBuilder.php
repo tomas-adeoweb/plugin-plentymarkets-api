94f996c003413a99ed934d5c43bf190142e02741
Tomas Semaška <tomas.semaska@adeoweb.lt>
2018-03-08 14:14:22 +0200

    Fix

--- src/Api/Request/RequestBuilder.php
+++ src/Api/Request/RequestBuilder.php
@@ -2,15 +2,56 @@
 
 namespace Findologic\PluginPlentymarketsApi\Api\Request;
 
+use Findologic\PluginPlentymarketsApi\Constants\Plugin;
+use Plenty\Plugin\ConfigRepository;
+use Plenty\Plugin\Log\LoggerFactory;
+
 /**
  * Class RequestBuilder
  * @package Findologic\Api\Request
  */
 class RequestBuilder
 {
+    /**
+     * @var ConfigRepository
+     */
+    protected $configRepository;
+
+    /**
+     * @var \Plenty\Log\Contracts\LoggerContract
+     */
+    protected $logger;
+
+    public function __construct(ConfigRepository $configRepository, LoggerFactory $loggerFactory)
+    {
+        $this->configRepository = $configRepository;
+        $this->logger = $loggerFactory->getLogger(Plugin::PLUGIN_NAMESPACE, Plugin::PLUGIN_IDENTIFIER);
+    }
+
+    /**
+     * @param Request $request
+     * @param null $searchQuery
+     * @return mixed|null
+     */
     public function build($request, $searchQuery = null)
     {
+        /** @var Request $request */
         $request = pluginApp(Request::class);
+
+        $request = $this->setDefaultValues($request);
+
+        return $request;
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function setDefaultValues($request)
+    {
+        $request->setUrl($this->configRepository->get(Plugin::CONFIG_URL));
+        $request->setParam('outputAdapter', Plugin::API_OUTPUT_ADAPTER);
+        $request->setParam('shopkey', $this->configRepository->get(Plugin::CONFIG_SHOPKEY));
 
         return $request;
     }
