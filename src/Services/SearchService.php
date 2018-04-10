c140109cedaadbce62e7300346674249f49482e8
Tomas Semaška <tomas.semaska@adeoweb.lt>
2018-04-10 17:04:03 +0300

    Fix

--- src/Services/SearchService.php
+++ src/Services/SearchService.php
@@ -92,16 +92,11 @@ class SearchService implements SearchServiceInterface
     {
         $request = $this->requestBuilder->buildAliveRequest();
         $response = $this->client->call($request);
-        $message = '';
 
-        if (is_array($response)) {
-            foreach ($response as $key => $value) {
-                $message .= ' Key: ' . $key . ' Value: ' . $value;
-            }
-        }
+        $this->logger->critical('Alive test.', $response);
 
         if ($response != Plugin::API_ALIVE_RESPONSE_BODY) {
-            throw new AliveException('Server is not alive! Response : ' . $message);
+            throw new AliveException('Server is not alive!');
         }
     }
 }
